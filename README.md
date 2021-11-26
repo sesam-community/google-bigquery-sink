77b165179fbc3e5a48e62e952dc5c9b26a5972ac
Mikkel <mikkel.m.bakken@gmail.com>
2021-11-26 12:14:52 +0100

    Changed to use dml to create a new table

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Code that writes to big query is in SmallScale folder.
 Run it with "python3 test.py". The json object pushed is currently hard coded to the program.
+
+Currently using dml to create a new table. Put the path of your own bigquery authentication key 
+in the credentials_path variable. The key needs the administrator role to create a new table.
--- SmallScale/test.py
+++ SmallScale/test.py
@@ -1,19 +1,34 @@
 from google.cloud import bigquery
+import google
 import os
 
-credentials_path = '/home/mikkel/Desktop/BigQueryMicroservice/BigQueryMicroservice/SmallScale/bigquery-microservice-6ad7d894bab7.json'
+credentials_path = '/home/mikkel/Desktop/BigQueryMicroservice/BigQueryMicroservice/SmallScale/bigquery-microservice-8767565ff502.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 client = bigquery.Client()
 table_id = 'bigquery-microservice.tesset.testset-table'
 
-rows_to_insert = [
-    {'id':'1', 'value':1, 'comment':'Comments are optional'}
-]
+dml_statement = "CREATE TABLE dmlCreatedTable (testId INTEGER, testValue STRING, testComment STRING);"
 
-errors = client.insert_rows_json(table_id, rows_to_insert)
+#dml_statement  = "INSERT INTO bigquery-microservice.tesset.testset-table\n(id, value, comment)\nVALUES ('2',25,'Another test value'),\n('3',14,'Yet another test value'"
 
-if errors == []:
-    print('New rows have been added to table')
-else:
-    print(f'Errors occured while adding rows to table: {errors}')
+query_job = client.query(dml_statement)
+    #INSERT bigquery-microservice.tesset.testset-table (id, value, comment)
+    #VALUES ('2',25,'Another test value'),
+    #('3',14,'Yet another test value');
+#)
+#try:
+query_job.result()
+#except:
+#    print('An error occured')
+
+#rows_to_insert = [
+#    {'id':'1', 'value':1, 'comment':'Comments are optional'}
+#]
+
+#errors = client.insert_rows_json(table_id, rows_to_insert)
+
+#if errors == []:
+#    print('New rows have been added to table')
+#else:
+#    print(f'Errors occured while adding rows to table: {errors}')
