724fd374910efaa699877a3be3b87abee1e388fa
Mikkel <mikkel.m.bakken@gmail.com>
2021-11-29 15:19:22 +0100

    Wrote the query so it can create a temporary table, merge that table to an existing one, then delete the temp table.

--- SmallScale/test.py
+++ SmallScale/test.py
@@ -6,9 +6,35 @@ credentials_path = '/home/mikkel/Desktop/BigQueryMicroservice/BigQueryMicroservi
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
 client = bigquery.Client()
-table_id = 'bigquery-microservice.tesset.testset-table'
+#table_id = 'bigquery-microservice.tesset.testset-table'
 
-dml_statement = "CREATE TABLE dmlCreatedTable (testId INTEGER, testValue STRING, testComment STRING);"
+table_id = 'testsetTable'
+cntr = 2
+
+dataset_name = client.list_tables('bigquery-microservice.tesset')
+
+for ele in dataset_name:
+    if (table_id + str(cntr)) == ele.table_id:
+        cntr += 1
+
+new_table = table_id + str(cntr)
+
+dml_statement = f"""CREATE TABLE bigquery-microservice.tesset.{new_table}
+(id INTEGER, value INTEGER, comment STRING);
+INSERT INTO bigquery-microservice.tesset.{new_table}
+VALUES (1,2,'This is a comment'),
+(2,2,'delete');
+MERGE INTO bigquery-microservice.tesset.{table_id} T
+USING bigquery-microservice.tesset.{new_table} S
+ON T.id = S.id
+WHEN NOT MATCHED THEN
+    INSERT (id, value, comment)
+    VALUES (id, value, comment)
+WHEN MATCHED AND S.comment = 'delete' THEN
+    DELETE;
+DROP TABLE bigquery-microservice.tesset.{new_table}
+"""
+
 
 #dml_statement  = "INSERT INTO bigquery-microservice.tesset.testset-table\n(id, value, comment)\nVALUES ('2',25,'Another test value'),\n('3',14,'Yet another test value'"
 
@@ -19,6 +45,7 @@ query_job = client.query(dml_statement)
 #)
 #try:
 query_job.result()
+
 #except:
 #    print('An error occured')
 
