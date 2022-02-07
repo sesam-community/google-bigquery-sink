d1083a4f2be5e31d26848b0ae3d37a49a48f744f
Tom Bech <tom.bech@gmail.com>
2022-02-07 15:35:43 +0100

    IS-5331: debug info

--- service/datasink-service.py
+++ service/datasink-service.py
@@ -262,6 +262,8 @@ def insert_entities_into_table(table, entities, wait_for_rows=True, prefix=''):
                 if not errors:
                     logger.info(f"{prefix}{len(chunk)} new rows have been added to table '%s'" % table)
                 else:
+                    from pprint import pprint
+                    logger.error("Example entity:\n%s" % pprint(entities[0]))
                     raise AssertionError(f"{prefix}Failed to insert json for table '%s':  %s" % (table, errors))
 
                 if ix == last_ix:
